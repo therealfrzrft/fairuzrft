ac63cf4ba3c9fdf448cf17f157c8357fc0bd9c49
Fairuz <fairuzrifat06@gmail.com>
2024-06-15 23:36:50 +0700

    Initial commit

---
+++ imfrzrft/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+strings: ["Student", "Mahasiswa/Pelajar", " Student/Mahasiswa/Pelajar in the Collage/University"],
+typeSpeed:100,
+backSpeed:100,
+backDelay:1000,
+loop: true
+})
